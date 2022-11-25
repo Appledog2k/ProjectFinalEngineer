2767251dd6fde920103e9f7f7137410ef56c6676
Appledog2k <Appledog2k@gmail.com>
2022-11-25 20:06:48 +0700

    addCategory

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -1,6 +1,7 @@
 
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProjectFinalEngineer.Models.AggregateCategory;
 using ProjectFinalEngineer.Models.AggregateContact;
 
 namespace ProjectFinalEngineer.Models;
@@ -20,19 +21,23 @@ public class AppDbContext : IdentityDbContext<AppUser>
     {
         base.OnModelCreating(modelBuilder);
 
-        //* Xử lý table name loại bỏ tiền tố ASP.NET
         foreach (var entityType in modelBuilder.Model.GetEntityTypes())
         {
             var tableName = entityType.GetTableName();
-            if (tableName == null)
-            {
-                Console.WriteLine("Không có tên bảng dữ liệu.");
-            }
             if (tableName.StartsWith("AspNet"))
             {
                 entityType.SetTableName(tableName.Substring(6));
             }
         }
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasIndex(c => c.Slug);
+        });
+
     }
+
     public DbSet<Contact> Contacts { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
 }
