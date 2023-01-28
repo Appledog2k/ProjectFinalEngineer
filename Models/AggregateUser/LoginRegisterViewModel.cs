4588a1e40f4f7ce0c1f5c412c5ea3ab0884284c7
appledog2k <appledog2k@gmail.com>
2023-01-29 00:19:21 +0700

    login register

---
+++ Models/AggregateUser/LoginRegisterViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectFinalEngineer.Models.AggregateUser
+{
+    public class LoginRegisterViewModel
+    {
+        public LoginViewModel LoginViewModel { get; set; }
+        public RegisterViewModel RegisterViewModel { get; set; }
+    }
+}
